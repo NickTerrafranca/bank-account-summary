03823fa1a3e844d69e0dbb24f3b5be8a75fcb098
Nicholas Terrafranca <nterrafranca@gmail.com>
2014-06-06 18:49:34 -0400

    Final UI DONE!

--- bank_account_executable.rb
+++ bank_account_executable.rb
@@ -1,19 +1,17 @@
 require_relative 'bank_transaction'
 require_relative 'bank_account'
+BALANCES = 'balances.csv'
+BANK_DATA = 'bank_data.csv'
+purchasing_account = BankAccount.new(BALANCES, BANK_DATA, 'Purchasing Account')
+business_checking = BankAccount.new(BALANCES, BANK_DATA, 'Business Checking')
 
-# I can load a CSV that contains my starting balances for all of my accounts
-# I can load a CSV that contains all of my bank transactions
-# For each account, the system outputs my starting balance, ending balance,
-#   and transaction history for the account in chronological order.
-
-# BankTransaction
-# deposit? that returns true if the transaction was a deposit as opposed to a withdrawal
-# summary that returns a string describing the transaction.
-
-
-# BankAccount
-# starting_balance that returns the starting balance loaded from the file
-# current_balance that returns the ending balance after all transactions have been processed
-# summary that returns an array of all of the transaction summaries.
-# Your BankAccount class should have an instance variable that contains an array of its
-# associated BankTransactions.
+puts "\n==== Purchasing Account ===="
+puts "Starting Balance: #{"%.02f" % purchasing_account.starting_balance}"
+puts "Ending Balance: #{"%.02f" % purchasing_account.current_balance}"
+puts "\n==== Summary ===="
+purchasing_account.summary
+puts "\n==== Business Checking ===="
+puts "Starting Balance: #{"%.02f" % business_checking.starting_balance}"
+puts "Ending Balance: #{"%.02f" % business_checking.current_balance}"
+puts "\n==== Summary ===="
+business_checking.summary
