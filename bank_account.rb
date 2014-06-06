5f942b5f64c32aa0caf4980537610565538b93fd
Nicholas Terrafranca <nterrafranca@gmail.com>
2014-06-06 14:59:45 -0400

    The method read_transaction_data is now creating BankTransaction objects.

--- bank_account.rb
+++ bank_account.rb
@@ -9,42 +9,46 @@ class BankAccount
     @balance_file = balance_file
     @transaction_file = transaction_file
     @account_name = account_name
+    @transactions = []
+    @balances = 0
   end
 
   def read_initial_balance
-    balances = []
     CSV.foreach(balance_file, headers: true) do |row|
-      if account_name == row['Account']
-        balances << row
+      if @account_name == row['Account']
+        @balances = row['Balance']
       end
     end
-    balances
+    @balances
   end
 
   def read_transaction_data
-    transactions = []
-    CSV.foreach(transaction_file, headers: true) do |row|
-      if account_name == row['Account']
-        transactions << row
+  CSV.foreach(transaction_file, headers: true) do |row|
+      if @account_name == row['Account']
+        @transactions << BankTransaction.new(row['Date'], row['Amount'], row['Description'], row['Account'])
       end
     end
-    transactions
-    binding.pry
+    @transactions
   end
 
-  # @array_of_transactions
-    # Your BankAccount class should have an instance variable that contains an array
-    # of its associated BankTransactions.
-
-    def starting_balance
-    # returns the starting balance loaded from the file
+  def starting_balance
+    ib = read_initial_balance
+    puts "$#{"%.02f" % ib[0]['Balance']}"
   end
+
+
+  def summary
+    # returns an array of all of the transaction summaries.
+
+  # $29.99   WITHDRAWAL  09/12/2013 - Amazon.com
+  # $500.33  DEPOSIT     09/14/2013 - Sales Deposit
+  # $35.19   WITHDRAWAL  09/15/2013 - Staples.com
+  end
+
+
 
   def current_balance
     # returns the ending balance after all transactions have been processed
   end
 
-  def summary
-    # returns an array of all of the transaction summaries.
-  end
 end
