024f77695f2041fa741adb5ec66dd94373dd0bdb
Nicholas Terrafranca <nterrafranca@gmail.com>
2014-06-06 14:28:50 -0400

    Added constructor and deposit? methods.

--- bank_transaction.rb
+++ bank_transaction.rb
@@ -1,1 +1,25 @@
+require 'pry'
+require 'csv'
 require_relative 'bank_account'
+
+class BankTransaction
+  def initialize(date, amount, description, account_name)
+    @date = date
+    @amount = amount
+    @description = description
+    @account_name = account_name
+  end
+
+  def deposit?
+    if @amount > 0
+      true
+    else
+      false
+    end
+  end
+
+  def summary
+    # returns a string describing the transaction
+  end
+end
+
