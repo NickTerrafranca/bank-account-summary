25e944fa76d48fc6dfb4920bcb4b2ccc71965814
Nicholas Terrafranca <nterrafranca@gmail.com>
2014-07-14 10:32:18 -0400

    Being OCD again.

--- bank_account_executable.rb
+++ bank_account_executable.rb
@@ -6,12 +6,12 @@ purchasing_account = BankAccount.new(BALANCES, BANK_DATA, 'Purchasing Account')
 business_checking = BankAccount.new(BALANCES, BANK_DATA, 'Business Checking')
 
 puts "\n==== Purchasing Account ===="
-puts "Starting Balance: #{"%.02f" % purchasing_account.starting_balance}"
-puts "Ending Balance: #{"%.02f" % purchasing_account.current_balance}"
+puts "Starting Balance: #{'%.02f' % purchasing_account.starting_balance}"
+puts "Ending Balance: #{'%.02f' % purchasing_account.current_balance}"
 puts "\n==== Summary ===="
 purchasing_account.summary
 puts "\n==== Business Checking ===="
-puts "Starting Balance: #{"%.02f" % business_checking.starting_balance}"
-puts "Ending Balance: #{"%.02f" % business_checking.current_balance}"
+puts "Starting Balance: #{'%.02f' % business_checking.starting_balance}"
+puts "Ending Balance: #{'%.02f' % business_checking.current_balance}"
 puts "\n==== Summary ===="
 business_checking.summary
